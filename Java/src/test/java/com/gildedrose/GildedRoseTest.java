79bb1bb1a4595bc4bb9bdab3c80909e600b0601a
Filip Lazarevic <filip.lazarevic984@gmail.com>
2025-06-11 16:10:00 +0200

    test: add test to verify Sulfuras does not change

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -52,10 +52,21 @@ class GildedRoseTest {
     void brieMaxQualityFifty() {
         Item[] items = new Item[] { new Item("Aged Brie", 2, 50) };
         GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(50, items[0].quality);
+    }
+
+    @Test
+    void sulfurasNeverChanges() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 0, 80) };
+        GildedRose app = new GildedRose(items);
     
         app.updateQuality();
     
-        assertEquals(50, items[0].quality);
+        assertEquals(80, items[0].quality);
+        assertEquals(0, items[0].sellIn);
     }
 
 }
