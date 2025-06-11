15738f8e410d72b2ee5a47b3cd89247a951ec31b
Filip Lazarevic <filip.lazarevic984@gmail.com>
2025-06-11 16:10:38 +0200

    test: add test for Backstage passes increasing and dropping to 0

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -62,11 +62,29 @@ class GildedRoseTest {
     void sulfurasNeverChanges() {
         Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 0, 80) };
         GildedRose app = new GildedRose(items);
-    
+
         app.updateQuality();
-    
+
         assertEquals(80, items[0].quality);
         assertEquals(0, items[0].sellIn);
+    }
+
+    @Test
+    void backstagePassesIncreaseAndDropToZero() {
+        Item[] passes = new Item[] {
+            new Item("Backstage passes to a TAFKAL80ETC concert", 11, 20),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20)
+        };
+    
+        GildedRose app = new GildedRose(passes);
+        app.updateQuality();
+    
+        assertEquals(21, passes[0].quality); // +1
+        assertEquals(22, passes[1].quality); // +2
+        assertEquals(23, passes[2].quality); // +3
+        assertEquals(0,  passes[3].quality); // 0 after concert
     }
 
 }
